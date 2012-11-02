57fba292526e9b4487f70e7588b89e0459e5dafe
Felix Kolbe <felix.kolbe@informatik.haw-hamburg.de>
2012-11-02 11:35:54 +0100

    Added warning in output to remind users of cleanup's side effects.

--- tools/rosnode/src/rosnode/__init__.py
+++ tools/rosnode/src/rosnode/__init__.py
@@ -447,8 +447,9 @@ def rosnode_cleanup():
         master = rosgraph.Master(ID)
         print("Unable to contact the following nodes:")
         print('\n'.join(' * %s'%n for n in unpinged))
-        print("cleanup will purge all information about these nodes from the master")
-        print("Please type y or n to continue")
+        print("Warning: these might include alive and functioning nodes, e.g. in unstable networks.")
+        print("Cleanup will purge all information about these nodes from the master.")
+        print("Please type y or n to continue:")
         input = sys.stdin.readline()
         while not input.strip() in ['y', 'n']:
             input = sys.stdin.readline()
