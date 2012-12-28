fd45f5ad5a1380b82c2022a0391ceb7e43283003
Dirk Thomas <dthomas@willowgarage.com>
2012-12-27 22:31:28 -0800

    moved rosparam tests to separate package to break circular deps

--- tools/rosparam/setup.py
+++ tools/rosparam/setup.py
@@ -6,8 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rosparam'],
     package_dir={'': 'src'},
-    scripts=['scripts/rosparam'],
-    requires=['genmsg', 'genpy', 'roslib', 'rospkg']
+    scripts=['scripts/rosparam']
 )
 
 setup(**d)
