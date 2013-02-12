49489d4cbbb999798e7072cdc8c6d987c617ce3e
Dan Lazewatsky <dan@lazewatsky.com>
2013-02-12 11:42:56 -0600

    added command line parameter to print out launch args

---
+++ tools/roslaunch/src/roslaunch/arg_dump.py
@@ -0,0 +1,62 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2009, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+"""
+Utility module of roslaunch that computes the command-line arguments
+for a launch file.
+"""
+
+import roslaunch.xmlloader
+from roslaunch.core import RLException
+from roslaunch.config import load_config_default
+
+def get_args(roslaunch_files):
+    loader = roslaunch.xmlloader.XmlLoader(resolve_anon=False)
+    config = load_config_default(roslaunch_files, None, loader=loader, verbose=False, assign_machines=False)
+    return loader.root_context.resolve_dict['arg']
+
+def dump_args(roslaunch_files):
+    """
+    Print list of args to screen. Will cause system exit if exception
+    occurs. This is a subroutine for the roslaunch main handler.
+
+    @param roslaunch_files: list of launch files to load
+    @type  roslaunch_files: str
+    """
+    try:
+        for arg in sorted(get_args(roslaunch_files).items()):
+        	print arg[0]
+    except RLException as e:
+        print >> sys.stderr, str(e)
+        sys.exit(1)
