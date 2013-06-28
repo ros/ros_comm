a330b07e738e1fecfaec993cfa9bc9bf8374693e
Jonathan Bohren <jbo@jhu.edu>
2013-06-28 16:01:50 -0400

    proper escaping for ROS_PACKAGE_NAME definition

--- tools/rosconsole/cmake/rosconsole-extras.cmake
+++ tools/rosconsole/cmake/rosconsole-extras.cmake
@@ -1,4 +1,4 @@
 # ros_comm/tools/rosconsole/cmake/rosconsole-extras.cmake
 
 # add ROS_PACKAGE_NAME define required by the named logging macros
-add_definitions(-DROS_PACKAGE_NAME=\"${PROJECT_NAME}\")
+add_definitions(-DROS_PACKAGE_NAME="\\"${PROJECT_NAME}\\"")
