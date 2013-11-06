d4dbfa7e343b91e2e9b76d8e0b86bb9928dd5c26
Johannes Meyer <johannes@intermodalics.eu>
2013-11-06 12:45:08 +0100

    roscpp: fixed name resolution of "/"

--- clients/roscpp/src/libros/names.cpp
+++ clients/roscpp/src/libros/names.cpp
@@ -107,7 +107,7 @@ std::string clean(const std::string& name)
     pos = clean.find("//", pos);
   }
 
-  if (*clean.rbegin() == '/')
+  if (clean.size() > 1 && *clean.rbegin() == '/')
   {
     clean.erase(clean.size() - 1, 1);
   }
