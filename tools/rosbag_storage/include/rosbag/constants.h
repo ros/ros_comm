be2877888ba8b175f679e3b6e96ac7a102911695
Tobias Opitz <development@topitz.de>
2013-11-21 00:03:28 +0100

    Support for older visual studio versions
    changed '#include <stdint.h>' based on
    roscpp_core/cpp_common/include/ros/types.h to support older versions of
    visual studio

--- tools/rosbag_storage/include/rosbag/constants.h
+++ tools/rosbag_storage/include/rosbag/constants.h
@@ -36,7 +36,22 @@
 #define ROSBAG_CONSTANTS_H
 
 #include <string>
-#include <stdint.h>
+
+// this is just for interoperability with visual studio, where the standard
+// integer types are not defined.
+
+#if defined(_MSC_VER) && (_MSC_VER < 1600 ) // MS express/studio 2008 or earlier
+  typedef          __int64  int64_t;
+  typedef unsigned __int64 uint64_t;
+  typedef          __int32  int32_t;
+  typedef unsigned __int32 uint32_t;
+  typedef          __int16  int16_t;
+  typedef unsigned __int16 uint16_t;
+  typedef          __int8    int8_t;
+  typedef unsigned __int8   uint8_t;
+#else
+  #include <stdint.h>
+#endif
 
 namespace rosbag {
 
--- tools/rosbag_storage/include/rosbag/stream.h
+++ tools/rosbag_storage/include/rosbag/stream.h
@@ -35,8 +35,23 @@
 #ifndef ROSBAG_STREAM_H
 #define ROSBAG_STREAM_H
 
+// this is just for interoperability with visual studio, where the standard
+// integer types are not defined.
+
+#if defined(_MSC_VER) && (_MSC_VER < 1600 ) // MS express/studio 2008 or earlier
+  typedef          __int64  int64_t;
+  typedef unsigned __int64 uint64_t;
+  typedef          __int32  int32_t;
+  typedef unsigned __int32 uint32_t;
+  typedef          __int16  int16_t;
+  typedef unsigned __int16 uint16_t;
+  typedef          __int8    int8_t;
+  typedef unsigned __int8   uint8_t;
+#else
+  #include <stdint.h>
+#endif
+
 #include <ios>
-#include <stdint.h>
 #include <string>
 
 #include <boost/shared_ptr.hpp>
