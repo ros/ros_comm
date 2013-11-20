889986292562c67e644ebd80888e256ec2e65155
Tobias Opitz <development@topitz.de>
2013-11-21 00:25:50 +0100

    Support for older visual studio versions
    changed '#include <stdint.h>' based on
    roscpp_core/cpp_common/include/ros/types.h to support older versions of
    visual studio

--- tools/rosbag_storage/src/bag.cpp
+++ tools/rosbag_storage/src/bag.cpp
@@ -30,11 +30,23 @@
 #include "rosbag/query.h"
 #include "rosbag/view.h"
 
-#if defined(_MSC_VER)
+// this is just for interoperability with visual studio, where the standard
+// integer types are not defined.
+#if defined(_MSC_VER) && (_MSC_VER < 1600 ) // MS express/studio 2008 or earlier
+  typedef          __int64  int64_t;
+  typedef unsigned __int64 uint64_t;
+  typedef          __int32  int32_t;
+  typedef unsigned __int32 uint32_t;
+  typedef          __int16  int16_t;
+  typedef unsigned __int16 uint16_t;
+  typedef          __int8    int8_t;
+  typedef unsigned __int8   uint8_t;
+#elseif defined(_MSC_VER)
   #include <stdint.h> // only on v2010 and later -> is this enough for msvc and linux?
 #else
   #include <inttypes.h>
 #endif
+
 #include <signal.h>
 #include <assert.h>
 #include <iomanip>
