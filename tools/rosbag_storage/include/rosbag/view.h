edace4a695f1072e41506639ff95fcbfac33cbde
Dirk Thomas <dthomas@osrfoundation.org>
2013-12-27 16:32:07 -0800

    rosbag_storage: avoid forward declare

--- tools/rosbag_storage/include/rosbag/view.h
+++ tools/rosbag_storage/include/rosbag/view.h
@@ -41,12 +41,9 @@
 #include "rosbag/message_instance.h"
 #include "rosbag/query.h"
 #include "rosbag/macros.h"
+#include "rosbag/structures.h"
 
 namespace rosbag {
-
-struct MessageRange;
-struct IndexEntry;
-struct ViewIterHelper;
 
 class ROSBAG_DECL View
 {
