ae73540db62367cfe628af9ddd3bb722748aa25d
Mirza A. Shah <mas644@gmail.com>
2014-01-15 04:09:37 -0800

    Python 3

--- tools/rosgraph/test/test_rosgraph_command_offline.py
+++ tools/rosgraph/test/test_rosgraph_command_offline.py
@@ -47,7 +47,7 @@ class TestRosgraphOffline(unittest.TestCase):
     def test_cmd_help(self):
         cmd = 'rosgraph'
         output = Popen([cmd, '-h'], stdout=PIPE).communicate()[0]
-        self.assert_('Usage' in output)
+        self.assert_('Usage' in output.decode())
             
     def test_offline(self):
         cmd = 'rosgraph'
@@ -60,4 +60,4 @@ class TestRosgraphOffline(unittest.TestCase):
         msg = "ERROR: Unable to communicate with master!\n"
 
         output = Popen([cmd], **kwds).communicate()
-        self.assertEquals(msg, output[1])
+        self.assertEquals(msg, output[1].decode())
