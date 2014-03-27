9bebfd32a0bae71750d61adcf426a5c8d6aa034b
Dariush Forouher <forouher@iti.uni-luebeck.de>
2014-03-27 13:54:17 +0100

    Add topic/connection statistics to roscpp
    This patch adds a module to roscpp that monitors some metrics
    over every ROS connection (period, dropped messages, traffic, delay)
    and periodically sends out aggregated statistics data over a common
    topic /statistics. It is designed to be lightweight so it can be used
    in "production" environments.
    
    Statistics monitoring is disabled by default. It has to be enabled
    explicitly using "rosparam set enable_statistics true".

--- clients/roscpp/src/libros/subscription.cpp
+++ clients/roscpp/src/libros/subscription.cpp
@@ -657,6 +657,9 @@ uint32_t Subscription::handleMessage(const SerializedMessage& m, bool ser, bool 
     }
   }
 
+  // measure statistics
+  statistics_.callback(connection_header,name_,link->getCallerID(),m,link->getStats().bytes_received_,receipt_time, drops>0);
+
   // If this link is latched, store off the message so we can immediately pass it to new subscribers later
   if (link->isLatched())
   {
@@ -677,6 +680,8 @@ bool Subscription::addCallback(const SubscriptionCallbackHelperPtr& helper, cons
 {
   ROS_ASSERT(helper);
   ROS_ASSERT(queue);
+
+  statistics_.init(helper);
 
   // Decay to a real type as soon as we have a subscriber with a real type
   {
