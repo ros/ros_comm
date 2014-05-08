e60e8b9bf503aa6f3e3609552ad1c2b9f593cdae
Andreas Hermann <hermann@fzi.de>
2014-05-08 23:02:48 +0200

    Fixed the old Demux code and brought it back alive. Mainly followed the code from the mux. Added according service descriptions.

--- tools/topic_tools/src/demux.cpp
+++ tools/topic_tools/src/demux.cpp
@@ -1,9 +1,9 @@
 ///////////////////////////////////////////////////////////////////////////////
 // demux is a generic ROS topic demultiplexer: one input topic is fanned out
-// to 1 of N output topics. A service and topic subscription are provided
-// to select between the outputs.
+// to 1 of N output topics. A service is provided to select between the outputs
 //
-// Copyright (C) 2009, Morgan Quigley
+// Copyright (C) 2014, Andreas Hermann
+// Code copied and adapted from the "toppic mux" by Morgan Quigley
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -29,72 +29,271 @@
 // POSSIBILITY OF SUCH DAMAGE.
 /////////////////////////////////////////////////////////////////////////////
 
-#include "ros/node.h"
+
+#include <cstdio>
+#include <vector>
+#include <list>
+#include "ros/console.h"
 #include "std_msgs/String.h"
 #include "topic_tools/DemuxSelect.h"
+#include "topic_tools/DemuxAdd.h"
+#include "topic_tools/DemuxList.h"
+#include "topic_tools/DemuxDelete.h"
 #include "topic_tools/shape_shifter.h"
+#include "topic_tools/parse.h"
 
 using std::string;
 using std::vector;
-using std::map;
+using std::list;
 using namespace topic_tools;
 
-static std_msgs::String g_sel_msg;
-static vector<string> g_output_topics, g_selected;
-static bool g_advertised = false;
-static ros::Node *g_node = NULL;
-static ShapeShifter g_in_msg;
+const static string g_none_topic = "__none";
+
+static ros::NodeHandle *g_node = NULL;
+
+static string g_input_topic;
+static ros::Subscriber g_sub; // the input toppic
+static ros::Publisher g_pub_selected; // publishes name of selected publisher toppic
+
+struct pub_info_t
+{
+  std::string topic_name;
+  ros::Publisher *pub;
+};
+
+void in_cb(const boost::shared_ptr<ShapeShifter const>& msg);
+
+static list<struct pub_info_t> g_pubs; // the list of publishers
+static list<struct pub_info_t>::iterator g_selected = g_pubs.end();
 
 bool sel_srv_cb( topic_tools::DemuxSelect::Request  &req,
                  topic_tools::DemuxSelect::Response &res )
 {
-  res.prev_topics = g_selected;
-  g_selected = req.topics;
-  printf("enabling demux on these topics:\n");
-  for (size_t i = 0; i < g_selected.size(); ++i)
-    printf("  %s\n", g_selected[i].c_str());
+  bool ret = false;
+  if (g_selected != g_pubs.end()) {
+    res.prev_topic = g_selected->topic_name;
+
+    // Unregister old topic
+    ROS_INFO("Unregistering %s", res.prev_topic.c_str());
+    if (g_selected->pub)
+	  g_selected->pub->shutdown();
+    delete g_selected->pub;
+    g_selected->pub = NULL;
+  }
+  else
+    res.prev_topic = string("");
+
+  // see if it's the magical '__none' topic, in which case we open the circuit
+  if (req.topic == g_none_topic)
+  {
+    ROS_INFO("demux selected to no output.");
+
+    g_selected = g_pubs.end();
+    ret = true;
+  }
+  else
+  {
+    ROS_INFO("trying to switch demux to %s", req.topic.c_str());
+    // spin through our vector of inputs and find this guy
+    for (list<struct pub_info_t>::iterator it = g_pubs.begin();
+	 it != g_pubs.end();
+	 ++it)
+    {
+      if (ros::names::resolve(it->topic_name) == ros::names::resolve(req.topic))
+      {
+        g_selected = it;
+        ROS_INFO("demux selected output: [%s]", it->topic_name.c_str());
+        ret = true;
+      }
+    }
+    if(!ret)
+    {
+    	ROS_WARN("Failed to switch to non-existing topic %s in demux.", req.topic.c_str());
+    }
+  }
+
+  if(ret)
+  {
+    std_msgs::String t;
+    t.data = req.topic;
+    g_pub_selected.publish(t);
+  }
+
+  return ret;
+}
+
+void in_cb(const boost::shared_ptr<ShapeShifter const>& msg)
+{
+  ROS_INFO("Received an incoming msg ...");
+  // when a message is incoming, check, if the requested publisher is already existing.
+  // if not, create it with the information available from the incoming message.
+  if(!g_selected->pub)
+  {
+	  try
+	  {
+		  g_selected->pub = new ros::Publisher(msg->advertise(*g_node, g_selected->topic_name, 10, false));
+	  }
+	  catch(ros::InvalidNameException& e)
+	  {
+		ROS_WARN("failed to add topic %s to demux, because it's an invalid name: %s",
+				g_selected->topic_name.c_str(), e.what());
+		return;
+	  }
+
+	  ROS_INFO("Added publisher %s to demux! Sleeping for 0.5 secs.", g_selected->topic_name.c_str());
+	  ros::Duration(0.5).sleep(); // sleep for half a second
+  }
+
+  // finally: send out the message over the active publisher
+  g_selected->pub->publish(msg);
+  ROS_INFO("... and sent it out again!");
+}
+
+bool list_topic_cb(topic_tools::DemuxList::Request& req,
+	 	   topic_tools::DemuxList::Response& res)
+{
+  for (list<struct pub_info_t>::iterator it = g_pubs.begin();
+       it != g_pubs.end();
+       ++it)
+  {
+    res.topics.push_back(it->topic_name);
+  }
+
   return true;
 }
 
-void in_cb()
+bool add_topic_cb(topic_tools::DemuxAdd::Request& req,
+		  topic_tools::DemuxAdd::Response& res)
 {
-  if (!ShapeShifter::typed)
+  // Check that it's not already in our list
+  ROS_INFO("trying to add %s to demux", req.topic.c_str());
+
+  // Can't add the __none topic
+  if(req.topic == g_none_topic)
   {
-    printf("reading first message header, setting datatype\n");
-    ShapeShifter::datatype = (*(g_in_msg.__connection_header))["type"];
-    ShapeShifter::md5 = (*(g_in_msg.__connection_header))["md5sum"];
-    ShapeShifter::msg_def = (*(g_in_msg.__connection_header))["message_definition"];
-    ShapeShifter::typed = true;
+    ROS_WARN("failed to add topic %s to demux, because it's reserved for special use",
+	     req.topic.c_str());
+    return false;
   }
-  if (!g_advertised)
+
+  // spin through our vector of inputs and find this guy
+  for (list<struct pub_info_t>::iterator it = g_pubs.begin();
+       it != g_pubs.end();
+       ++it)
   {
-    printf("advertising\n");
-    for (size_t i = 0; i < g_output_topics.size(); i++)
-      g_node->advertise<ShapeShifter>(g_output_topics[i], 10);
-    g_advertised = true;
+    if (ros::names::resolve(it->topic_name) == ros::names::resolve(req.topic))
+    {
+      ROS_WARN("tried to add a topic that demux was already publishing: [%s]",
+	       it->topic_name.c_str());
+      return false;
+    }
   }
-  for (size_t i = 0; i < g_selected.size(); i++)
-    g_node->publish(g_selected[i], g_in_msg);
+
+  struct pub_info_t pub_info;
+  pub_info.topic_name = ros::names::resolve(req.topic);
+  pub_info.pub = NULL;
+  g_pubs.push_back(pub_info);
+
+  ROS_INFO("PRE added %s to demux", req.topic.c_str());
+
+  return true;
+}
+
+bool del_topic_cb(topic_tools::DemuxDelete::Request& req,
+		  topic_tools::DemuxDelete::Response& res)
+{
+  // Check that it's in our list
+  ROS_INFO("trying to delete %s from demux", req.topic.c_str());
+  // spin through our vector of inputs and find this guy
+  for (list<struct pub_info_t>::iterator it = g_pubs.begin();
+       it != g_pubs.end();
+       ++it)
+  {
+    if (ros::names::resolve(it->topic_name) == ros::names::resolve(req.topic))
+    {
+      // Can't delete the currently selected input, #2863
+      if(it == g_selected)
+      {
+        ROS_WARN("tried to delete currently selected topic %s from demux", req.topic.c_str());
+        return false;
+      }
+      if (it->pub)
+        it->pub->shutdown();
+      delete it->pub;
+      g_pubs.erase(it);
+      ROS_INFO("deleted topic %s from demux", req.topic.c_str());
+      return true;
+    }
+  }
+
+  ROS_WARN("tried to delete non-published topic %s from demux", req.topic.c_str());
+  return false;
 }
 
 int main(int argc, char **argv)
 {
-  if (argc < 4)
+  vector<string> args;
+  ros::removeROSArgs(argc, (const char**)argv, args);
+
+  if (args.size() < 3)
   {
-    printf("\nusage: demux IN_TOPIC OUT_TOPIC1 OUT_TOPIC2 [...]\n");
+    printf("\nusage: demux IN_TOPIC OUT_TOPIC1 [OUT_TOPIC2 [...]]\n\n");
     return 1;
   }
-  string in_topic = string(argv[1]);
-  ros::init(argc, argv, in_topic + string("_demux"));
-  for (int i = 2; i < argc; i++)
-    g_output_topics.push_back(argv[i]);
-  ros::Node n(in_topic + string("_demux"));
+  std::string topic_name;
+  if(!getBaseName(args[1], topic_name))
+    return 1;
+  ros::init(argc, argv, topic_name + string("_demux"),
+            ros::init_options::AnonymousName);
+  vector<string> topics;
+  for (unsigned int i = 2; i < args.size(); i++)
+    topics.push_back(args[i]);
+  ros::NodeHandle n;
   g_node = &n;
-  n.advertiseService(in_topic + string("_demux_select"), sel_srv_cb);
-  g_in_msg.topic = in_topic;
-  n.subscribe(in_topic, g_in_msg, in_cb, 10);
-  g_selected.push_back(g_output_topics[0]); // select the first one to start
+  g_input_topic = args[1];
+  // Put our API into the "demux" namespace, which the user should usually remap
+  ros::NodeHandle pnh("~");
+
+  // Latched publisher for selected output topic name
+  g_pub_selected = pnh.advertise<std_msgs::String>(string("selected"), 1, true);
+
+  for (size_t i = 0; i < topics.size(); i++)
+  {
+    struct pub_info_t pub_info;
+    pub_info.topic_name = ros::names::resolve(topics[i]);
+    pub_info.pub = NULL;
+    g_pubs.push_back(pub_info);
+    ROS_INFO("PRE added %s to demux", topics[i].c_str());
+  }
+  g_selected = g_pubs.begin(); // select first topic to start
+  std_msgs::String t;
+  t.data = g_selected->topic_name;
+  g_pub_selected.publish(t);
+
+  // Create the one subscriber
+  g_sub = ros::Subscriber(g_node->subscribe<ShapeShifter>(g_input_topic, 10, boost::bind(in_cb, _1)));
+
+
+  // New service
+  ros::ServiceServer ss_select = pnh.advertiseService(string("select"), sel_srv_cb);
+  ros::ServiceServer ss_add = pnh.advertiseService(string("add"), add_topic_cb);
+  ros::ServiceServer ss_list = pnh.advertiseService(string("list"), list_topic_cb);
+  ros::ServiceServer ss_del = pnh.advertiseService(string("delete"), del_topic_cb);
+
+  // Run
   ros::spin();
+
+  // Destruction
+  for (list<struct pub_info_t>::iterator it = g_pubs.begin();
+       it != g_pubs.end();
+       ++it)
+  {
+    if (it->pub)
+      it->pub->shutdown();
+    delete it->pub;
+  }
+
+  g_pubs.clear();
   return 0;
 }
 
