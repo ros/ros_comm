db4801b430c74e8b5a93e1c7441495176664b67b
Maxime Rio <maxime.rio@inria.fr>
2015-03-11 09:41:43 +0100

    force clock publishing in case of to heavy load on rosbag play (fix #82)
    rostest for forced clock publishing in case of heavy load on rosbag play
    
    code style corrections
    
    better use of Unittest assertions, and simpler code in forceClock method
    
    fixed bug in keep-alive mode of rosbag, to get a clock after playing a heavy bag
    
    modified test bag url
    
    new test bag, lighter

---
+++ test/test_rosbag/test/clock_sub.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import unittest
+
+import rospy
+import rostest
+from std_msgs.msg import String
+from rosgraph_msgs.msg import Clock
+
+
+class ClockSub(unittest.TestCase):
+
+    def msg_cb(self, msg):
+        pass
+
+    def clock_cb(self, clock):
+        self.success = True
+
+    def test_clock_sub(self):
+        rospy.init_node('clock_sub')
+
+        self.success = False
+
+        rospy.Subscriber("blabla", String, self.msg_cb)
+        rospy.Subscriber("clock", Clock, self.clock_cb)
+
+        time.sleep(2)
+
+        self.assertTrue(self.success)
+
+if __name__ == '__main__':
+    rostest.rosrun('rosbag', 'clock_sub', ClockSub, sys.argv)
