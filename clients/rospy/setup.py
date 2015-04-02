45329a71d174a25c253eae7e0158a3fc0464c9d0
Brian Gerkey <gerkey@osrfoundation.org>
2015-04-02 14:13:12 -0700

    Moved the new rosconsole tool into rospy, because it depends on rospy, which depends on the rosconsole package (via roscpp).

--- clients/rospy/setup.py
+++ clients/rospy/setup.py
@@ -6,6 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rospy', 'rospy.impl'],
     package_dir={'': 'src'},
+    scripts=['scripts/rosconsole'],
     requires=['genpy', 'numpy', 'rosgraph', 'roslib', 'rospkg']
 )
 
