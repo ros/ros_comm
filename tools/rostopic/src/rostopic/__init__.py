eb3f805fc5477a36342e7f1c2a4be76421c5beda
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-12-15 13:29:15 +0900

    [rostopic] Get parent topics in rostopic hz
    Parent topics means that topics to which the specified topic subscribes to
    Depends on #712

--- tools/rostopic/src/rostopic/__init__.py
+++ tools/rostopic/src/rostopic/__init__.py
@@ -197,6 +197,34 @@ def _sleep(duration):
     rospy.rostime.wallsleep(duration)
 
 
+def get_parent_topics(topic, parent_topics=None, impl=False):
+    """Get topics to which the specified topic subscribes to.
+
+    @type topic: ``str``
+    @param topic: topic name
+    @type parent_topics: ``NoneType`` or ``list``
+    @param parent_topics: list of parent topics which already got.
+    @type impl: ``bool``
+    @param impl: search parent topics recursively or not
+    """
+    master = rosgraph.Master('/rostopic')
+    publications, subscriptions, _ = master.getSystemState()
+    if parent_topics is None:
+        parent_topics = []
+    pub_nodes = [l for t, l in publications if t == topic]
+    if not pub_nodes:
+        return parent_topics
+    for node in pub_nodes[0]:
+        subs = [t for t, l in subscriptions if node in l]
+        subs = filter(lambda x: x not in parent_topics, subs)  # get unique
+        parent_topics.extend(subs)
+        if not impl:
+            return parent_topics
+        for sub in subs:
+            parent_topics = get_parent_topics(sub, parent_topics, impl=impl)
+    return parent_topics
+
+
 def _rostopic_hz(topics, window_size=-1, filter_expr=None):
     """
     Periodically print the publishing rate of a topic to console until
@@ -1249,6 +1277,7 @@ def _rostopic_cmd_hz(argv):
     parser.add_option("--filter",
                       dest="filter_expr", default=None,
                       help="only measure messages matching the specified Python expression", metavar="EXPR")
+    parser.add_option("--search-parent", action="store_true", help="search parent topics and check the hz")
 
     (options, args) = parser.parse_args(args)
     if len(args) == 0:
@@ -1263,6 +1292,13 @@ def _rostopic_cmd_hz(argv):
         parser.error("window size must be an integer")
 
     topics = [rosgraph.names.script_resolve_name('rostopic', t) for t in args]
+    if options.search_parent:
+        # get all parent topics
+        parent_topics = []
+        for topic in topics:
+            parent_topics.extend(get_parent_topics(topic, impl=True))
+        parent_topics = list(set(parent_topics))
+        topics.extend(parent_topics)
 
     # #694
     if options.filter_expr:
