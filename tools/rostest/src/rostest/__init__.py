ros/ros_comm

commit 4c837a7f9cd72dc1c170754bf5231ad2628d4817

rostest can load tests from a dotted name (#722)

tools/rostest/src/rostest/__init__.py
@@ -109,8 +109,8 @@ def rosrun(package, test_name, test, sysargs=None):
     @type  package: str
     @param test_name: name of test that is being run
     @type  test_name: str
-    @param test: test class 
-    @type  test: unittest.TestCase
+    @param test: a test case instance or a name resolving to a test case or suite
+    @type  test: unittest.TestCase, or string
     @param sysargs: command-line args. If not specified, this defaults to sys.argv. rostest
       will look for the --text and --gtest_output parameters
     @type  sysargs: list
@@ -133,7 +133,12 @@ def rosrun(package, test_name, test, sysargs=None):
     import unittest
     import rospy
     
-    suite = unittest.TestLoader().loadTestsFromTestCase(test)
+    suite = None
+    if issubclass(test, unittest.TestCase):
+        suite = unittest.TestLoader().loadTestsFromTestCase(test)
+    elif:
+        suite = unittest.TestLoader().loadTestsFromName(test)
+
     if text_mode:
         result = unittest.TextTestRunner(verbosity=2).run(suite)
     else:

tools/rostest/test/dotname_cases.py
@@ -1,0 +1,36 @@
+import unittest
+
+
+class CaseA(unittest.TestCase):
+
+    def runTest(self):
+        self.assertTrue(True)
+
+
+class CaseB(unittest.TestCase):
+
+    def runTest(self):
+        self.assertTrue(True)
+
+
+class DotnameLoadingSuite(unittest.TestSuite):
+
+    def __init__(self):
+        super(DotnameLoadingSuite, self).__init__()
+        self.addTest(CaseA())
+        self.addTest(CaseB())
+
+
+class DotnameLoadingTest(unittest.TestCase):
+
+    def test_a(self):
+        self.assertTrue(True)
+
+    def test_b(self):
+        self.assertTrue(True)
+
+
+class NotTestCase():
+
+    def not_test(self):
+        pass

tools/rostest/test/test_dotname.py
@@ -1,0 +1,52 @@
+#!/usr/bin/env python
+
+# This file should be run using a non-ros unit test framework such as nose using
+# nosetests test_dotname.py.
+
+import unittest
+import rostest
+from dotname_cases import DotnameLoadingTest, NotTestCase, DotnameLoadingSuite
+
+
+class TestDotnameLoading(unittest.TestCase):
+
+    def test_class_basic(self):
+        rostest.rosrun('test_rostest', 'test_class_basic', DotnameLoadingTest)
+
+    def test_class_dotname(self):
+        rostest.rosrun('test_rostest', 'test_class_dotname', 'dotname_cases.DotnameLoadingTest')
+
+    def test_method_dotname(self):
+        rostest.rosrun('test_rostest', 'test_method_dotname', 'dotname_cases.DotnameLoadingTest.test_a')
+
+    def test_suite_dotname(self):
+        rostest.rosrun('test_rostest', 'test_suite_dotname', 'dotname_cases.DotnameLoadingSuite')
+
+    def test_class_basic_nottest(self):
+        # class which exists but is not a TestCase
+        with self.assertRaises(SystemExit):
+            rostest.rosrun('test_rostest', 'test_class_basic_nottest', NotTestCase)
+
+    def test_suite_basic(self):
+        # can't load suites with the basic loader
+        with self.assertRaises(SystemExit):
+            rostest.rosrun('test_rosunit', 'test_suite_basic', DotnameLoadingSuite)
+
+    def test_class_dotname_nottest(self):
+        # class which exists but is not a valid test
+        with self.assertRaises(TypeError):
+            rostest.rosrun('test_rostest', 'test_class_dotname_nottest', 'dotname_cases.NotTestCase')
+
+    def test_class_dotname_noexist(self):
+        # class which does not exist in the module
+        with self.assertRaises(AttributeError):
+            rostest.rosrun('test_rostest', 'test_class_dotname_noexist', 'dotname_cases.DotnameLoading')
+
+    def test_method_dotname_noexist(self):
+        # method which does not exist in the class
+        with self.assertRaises(AttributeError):
+            rostest.rosrun('test_rostest', 'test_method_dotname_noexist', 'dotname_cases.DotnameLoadingTest.not_method')
+
+
+if __name__ == '__main__':
+    unittest.main()

commit 05e7bd40669139fb0b7a761cc202f314745e30b9

fix syntax error introduced in 4c837a7f9cd72dc1c170754bf5231ad2628d4817

tools/rostest/src/rostest/__init__.py
@@ -136,7 +136,7 @@ def rosrun(package, test_name, test, sysargs=None):
     suite = None
     if issubclass(test, unittest.TestCase):
         suite = unittest.TestLoader().loadTestsFromTestCase(test)
-    elif:
+    else:
         suite = unittest.TestLoader().loadTestsFromName(test)
 
     if text_mode: