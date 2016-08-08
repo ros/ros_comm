4632a580107cd446ef4c41000087611b3b31bf60
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-08-09 05:57:05 +0900

    [message_filters] Fast approximate time synchronization in message_filters (pure python) (#802)
    * Fast approximate time synchronization in message_filters
    
    * Don't break the api of message_filters Synchronizers
    
    * Required changes to merge origin/kinetic-devel

--- utilities/message_filters/src/message_filters/__init__.py
+++ utilities/message_filters/src/message_filters/__init__.py
@@ -34,6 +34,7 @@ import itertools
 import threading
 import rospy
 
+
 class SimpleFilter(object):
 
     def __init__(self):
@@ -202,9 +203,11 @@ class TimeSynchronizer(SimpleFilter):
 
     def connectInput(self, fs):
         self.queues = [{} for f in fs]
-        self.input_connections = [f.registerCallback(self.add, q) for (f, q) in zip(fs, self.queues)]
+        self.input_connections = [
+            f.registerCallback(self.add, q, i_q)
+            for i_q, (f, q) in enumerate(zip(fs, self.queues))]
 
-    def add(self, msg, my_queue):
+    def add(self, msg, my_queue, my_queue_index=None):
         self.lock.acquire()
         my_queue[msg.header.stamp] = msg
         while len(my_queue) > self.queue_size:
@@ -237,7 +240,7 @@ class ApproximateTimeSynchronizer(TimeSynchronizer):
         self.slop = rospy.Duration.from_sec(slop)
         self.allow_headerless = allow_headerless
 
-    def add(self, msg, my_queue):
+    def add(self, msg, my_queue, my_queue_index=None):
         if not hasattr(msg, 'header') or not hasattr(msg.header, 'stamp'):
             if not self.allow_headerless:
                 rospy.logwarn("Cannot use message filters with non-stamped messages. "
@@ -252,7 +255,31 @@ class ApproximateTimeSynchronizer(TimeSynchronizer):
         my_queue[stamp] = msg
         while len(my_queue) > self.queue_size:
             del my_queue[min(my_queue)]
-        for vv in itertools.product(*[list(q.keys()) for q in self.queues]):
+        # self.queues = [topic_0 {stamp: msg}, topic_1 {stamp: msg}, ...]
+        if my_queue_index is None:
+            search_queues = self.queues
+        else:
+            search_queues = self.queues[:my_queue_index] + \
+                self.queues[my_queue_index+1:]
+        # sort and leave only reasonable stamps for synchronization
+        stamps = []
+        for queue in search_queues:
+            topic_stamps = []
+            for s in queue:
+                stamp_delta = abs(s - stamp)
+                if stamp_delta > self.slop:
+                    continue  # far over the slop
+                topic_stamps.append((s, stamp_delta))
+            if not topic_stamps:
+                self.lock.release()
+                return
+            topic_stamps = sorted(topic_stamps, key=lambda x: x[1])
+            stamps.append(topic_stamps)
+        for vv in itertools.product(*[zip(*s)[0] for s in stamps]):
+            vv = list(vv)
+            # insert the new message
+            if my_queue_index is not None:
+                vv.insert(my_queue_index, stamp)
             qt = list(zip(self.queues, vv))
             if ( ((max(vv) - min(vv)) < self.slop) and
                 (len([1 for q,t in qt if t not in q]) == 0) ):
@@ -260,4 +287,5 @@ class ApproximateTimeSynchronizer(TimeSynchronizer):
                 self.signalMessage(*msgs)
                 for q,t in qt:
                     del q[t]
+                break  # fast finish after the synchronization
         self.lock.release()
