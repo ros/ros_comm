376ea5b19d9e65b1703ccd5718a31e651925dd3e
Sébastien Barthélémy <barthelemy@crans.org>
2017-03-17 00:35:40 +0100

    refactor test_rosbag_storage

--- test/test_rosbag_storage/src/create_and_iterate_bag.cpp
+++ test/test_rosbag_storage/src/create_and_iterate_bag.cpp
@@ -10,71 +10,71 @@
 #include "boost/foreach.hpp"
 #include <gtest/gtest.h>
 
-
-TEST(rosbag_storage, create_and_iterate_bag)
+void create_test_bag(const std::string &filename)
 {
-  const char* bag_filename = "/tmp/rosbag_storage_create_and_iterate_bag.bag";
+  rosbag::Bag bag;
+  bag.open(filename, rosbag::bagmode::Write);
+
+  std_msgs::String str;
+  str.data = std::string("foo");
+
+  std_msgs::Int32 i;
+  i.data = 42;
+
+  bag.write("chatter", ros::Time::now(), str);
+  bag.write("numbers", ros::Time::now(), i);
+
+  bag.close();
+}
+
+const char* bag_filename = "/tmp/rosbag_storage_create_and_iterate_bag.bag";
+
+TEST(rosbag_storage, iterate_bag)
+{
+  rosbag::Bag bag;
+  bag.open(bag_filename, rosbag::bagmode::Read);
+
+  std::vector<std::string> topics;
+  topics.push_back(std::string("chatter"));
+  topics.push_back(std::string("numbers"));
+
+  rosbag::View view(bag, rosbag::TopicQuery(topics));
+
+  BOOST_FOREACH(rosbag::MessageInstance const m, view)
   {
-    rosbag::Bag bag;
-    bag.open(bag_filename, rosbag::bagmode::Write);
-
-    std_msgs::String str;
-    str.data = std::string("foo");
-  
-    std_msgs::Int32 i;
-    i.data = 42;
-  
-    bag.write("chatter", ros::Time::now(), str);
-    bag.write("numbers", ros::Time::now(), i);
-
-    bag.close();
-  }
-
-  {
-    rosbag::Bag bag;
-    bag.open(bag_filename, rosbag::bagmode::Read);
-
-    std::vector<std::string> topics;
-    topics.push_back(std::string("chatter"));
-    topics.push_back(std::string("numbers"));
-
-    rosbag::View view(bag, rosbag::TopicQuery(topics));
-
-    BOOST_FOREACH(rosbag::MessageInstance const m, view)
+    std_msgs::String::ConstPtr s = m.instantiate<std_msgs::String>();
+    if (s != NULL)
     {
-      std_msgs::String::ConstPtr s = m.instantiate<std_msgs::String>();
-      if (s != NULL)
-      {
-        if(s->data == std::string("foo")) { 
-          printf("Successfully checked string foo\n");
-        }
-        else
-        {
-          printf("Failed checked string foo\n");
-          FAIL();
-        }
+      if(s->data == std::string("foo")) {
+        printf("Successfully checked string foo\n");
       }
-
-      std_msgs::Int32::ConstPtr i = m.instantiate<std_msgs::Int32>();
-      if (i != NULL)
+      else
       {
-        if (i->data == 42) { 
-          printf("Successfully checked value 42\n");
-        }
-        else
-        {
-          printf("Failed checked value 42.\n"); 
-          FAIL();
-        }
+        printf("Failed checked string foo\n");
+        FAIL();
       }
     }
 
-    bag.close();
+    std_msgs::Int32::ConstPtr i = m.instantiate<std_msgs::Int32>();
+    if (i != NULL)
+    {
+      if (i->data == 42) {
+        printf("Successfully checked value 42\n");
+      }
+      else
+      {
+        printf("Failed checked value 42.\n");
+        FAIL();
+      }
+    }
   }
+
+  bag.close();
 }
 
 int main(int argc, char **argv) {
     ros::Time::init();
+    create_test_bag(bag_filename);
 
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
