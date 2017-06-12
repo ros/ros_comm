960994155c810d04b833c386931684e5ce9dec40
Olga Lupuleac <olga_lupuleac@mail.ru>
2017-06-12 19:24:01 +0300

    fixed tests

--- tools/rosbag_info_pkg/all_tests.sh
+++ tools/rosbag_info_pkg/all_tests.sh
@@ -1,23 +1,17 @@
 #! /bin/bash
-#wget 'http://infinity.csail.mit.edu/data/2011/2011-01-27-07-49-54.bag'
-#mv 2011-01-27-07-49-54.bag test.bag
+wget 'http://infinity.csail.mit.edu/data/2011/2011-01-21-09-01-36.bag'
 echo > rosbag_info_log.txt
-echo > result.txt
-files=$(ls "/media/olga/Expansion Drive" | grep -E ".*\.bag")
+file=$(ls | grep -E ".*\.bag")
 chmod +x test.sh
-for file in $files
-do
-echo "$file"
 echo "$file" >> rosbag_info_log.txt
-#cp "/media/olga/Expansion Drive/$file" test.bag 
 sum_of_all=0
 test_num=0
 params=("path" "size" "version" "duration" "start" "end" "compression" "types" "topics" "messages" "indexed")
 for param in "${params[@]}"
 do
-    test_res=$(./test.sh "$param" "/media/olga/Expansion Drive/$file")
+    test_res=$(./test.sh "$param" "$file")
     echo "ratio $test_res" >> rosbag_info_log.txt
-    test_with_freq_res=$(./test.sh "$param" "/media/olga/Expansion Drive/$file" --freq)	
+    test_with_freq_res=$(./test.sh "$param" "$file" --freq)	
     echo "ratio $test_with_freq_res" >> rosbag_info_log.txt
     if [ "$test_res" == "FAIL" ] || [ "$test_with_freq_res" == "FAIL" ];then
          exit 1
@@ -27,5 +21,4 @@ do
 done
 echo "average ratio"
 echo "scale=2; $sum_of_all / $test_num" | bc -l
-#rm test.bag
-done
+
