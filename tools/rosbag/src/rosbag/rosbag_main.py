ros/ros_comm

commit d1d1711018b27bafc70f5b01a3febc8f3c0b4a98

expose rate-control-topic and rate-control-max-delay args to command line tool

tools/rosbag/src/rosbag/rosbag_main.py
@@ -222,6 +222,8 @@ def play_cmd(argv):
     parser.add_option("--pause-topics", dest="pause_topics", default=[],  callback=handle_pause_topics, action="callback", help="topics to pause on during playback")
     parser.add_option("--bags",  help="bags files to play back from")
     parser.add_option("--wait-for-subscribers",  dest="wait_for_subscribers", default=False, action="store_true", help="wait for at least one subscriber on each topic before publishing")
+    parser.add_option("--rate-control-topic", dest="rate_control_topic", default='', type='str', help="watch the given topic, and if the last publish was more than <rate-control-max-delay> ago, wait until the topic publishes again to continue playback")
+    parser.add_option("--rate-control-max-delay", dest="rate_control_max_delay", default=1.0, type='float', help="maximum time difference from <rate-control-topic> before pausing")
 
     (options, args) = parser.parse_args(argv)
 
@@ -268,6 +270,12 @@ def play_cmd(argv):
     # prevent bag files to be passed as --topics or --pause-topics
     if options.topics or options.pause_topics:
         cmd.extend(['--bags'])
+
+    if options.rate_control_topic:
+        cmd.extend(['--rate-control-topic', str(options.rate_control_topic)])
+
+    if options.rate_control_max_delay:
+        cmd.extend(['--rate-control-max-delay', str(options.rate_control_max_delay)])
 
     cmd.extend(args)
 

commit b5f9969c6ca51ff1bb777c993d7f7cd8ce4d9dea

Fix publishing of selected topics from bag file, regression from d1d1711
Selecting a subset of topics to publish from a bag file is not working:

$ rosbag play --topics /tf test.bag
Usage: rosbag play BAGFILE1 [BAGFILE2 BAGFILE3 ...]
rosbag: error: You must specify at least 1 bag file to play back.

$ rosbag play test.bag --topics /tf
[ INFO] [1504144739.942816979]: Opening --rate-control-max-delay
[FATAL] [1504144739.943025646]: Error opening file: --rate-control-max-delay

tools/rosbag/src/rosbag/rosbag_main.py
@@ -271,13 +271,13 @@ def play_cmd(argv):
     if options.topics or options.pause_topics:
         cmd.extend(['--bags'])
 
+    cmd.extend(args)
+
     if options.rate_control_topic:
         cmd.extend(['--rate-control-topic', str(options.rate_control_topic)])
 
     if options.rate_control_max_delay:
         cmd.extend(['--rate-control-max-delay', str(options.rate_control_max_delay)])
-
-    cmd.extend(args)
 
     old_handler = signal.signal(
         signal.SIGTERM,