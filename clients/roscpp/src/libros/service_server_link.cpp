5f6f4b2c310f54096215e8dbf0d6e4ca45371264
Dmitry Kargin <dv_frost@mail.ru>
2017-09-07 17:41:06 +0300

    Trying to ease merging process

--- clients/roscpp/src/libros/service_server_link.cpp
+++ clients/roscpp/src/libros/service_server_link.cpp
@@ -322,11 +322,6 @@ void ServiceServerLink::processNextCall()
   }
 }
 
-bool ServiceServerLink::call(const SerializedMessage& req, SerializedMessage& resp)
-{
-  return call(req, resp, -1);
-}
-
 bool ServiceServerLink::call(const SerializedMessage& req, SerializedMessage& resp, double timeout)
 {
   CallInfoPtr info(new CallInfo);
@@ -400,6 +395,11 @@ bool ServiceServerLink::call(const SerializedMessage& req, SerializedMessage& re
   }
 
   return info->success_;
+}
+
+bool ServiceServerLink::call(const SerializedMessage& req, SerializedMessage& resp)
+{
+  return call(req, resp, -1);
 }
 
 bool ServiceServerLink::isValid() const
