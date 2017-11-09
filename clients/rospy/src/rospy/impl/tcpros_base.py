ros/ros_comm

commit 423209eb04e70d4d6e3d0f3adf6c056e35400190

use python constants rather than hardcoded integers for error codes

clients/rospy/src/rospy/impl/tcpros_base.py
@@ -41,6 +41,7 @@ try:
 except ImportError:
     from io import StringIO, BytesIO #Python 3.x
     python3 = 1
+import errno
 import socket
 import logging
 
@@ -154,8 +155,8 @@ class TCPServer(object):
             except socket.timeout:
                 continue
             except IOError as e:
-                (errno, msg) = e.args
-                if errno == 4: #interrupted system call
+                (e_errno, msg) = e.args
+                if e_errno == errno.EINTR: #interrupted system call
                     continue
                 raise
             if self.is_shutdown:
@@ -568,7 +569,9 @@ class TCPROSTransport(Transport):
             if not isinstance(e, socket.error):
                 # FATAL: no reconnection as error is unknown
                 self.close()
-            elif not isinstance(e, socket.timeout) and e.errno not in [100, 101, 102, 103, 110, 112, 113]:
+            elif not isinstance(e, socket.timeout) and e.errno not in [
+                    errno.ENETDOWN, e.ENETUNREACH, errno.ENETRESET,
+                    errno.ECONNABORTED, errno.ETIMEDOUT, errno.EHOSTDOWN, errno.EHOSTUNREACH]:
                 # reconnect in follow cases, otherwise close the socket:
                 # 1. socket.timeout: on timeouts caused by delays on wireless links
                 # 2. ENETDOWN (100), ENETUNREACH (101), ENETRESET (102), ECONNABORTED (103):
@@ -683,20 +686,20 @@ class TCPROSTransport(Transport):
             self.stat_num_msg += 1
         except IOError as ioe:
             #for now, just document common errno's in code
-            (errno, msg) = ioe.args
-            if errno == 32: #broken pipe
+            (ioe_errno, msg) = ioe.args
+            if ioe_errno == errno.EPIPE: #broken pipe
                 logdebug("ERROR: Broken Pipe")
                 self.close()
-                raise TransportTerminated(str(errno)+msg)
+                raise TransportTerminated(str(ioe_errno)+msg)
             raise #re-raise
         except socket.error as se:
             #for now, just document common errno's in code
-            (errno, msg) = se.args
-            if errno == 32: #broken pipe
+            (se_errno, msg) = se.args
+            if se_errno == errno.EPIPE: #broken pipe
                 logdebug("[%s]: Closing connection [%s] due to broken pipe", self.name, self.endpoint_id)
                 self.close()
                 raise TransportTerminated(msg)
-            elif errno == 104: #connection reset by peer
+            elif se_errno == errno.ECONNRESET: #connection reset by peer
                 logdebug("[%s]: Peer [%s] has closed connection", self.name, self.endpoint_id) 
                 self.close()
                 raise TransportTerminated(msg)
@@ -704,7 +707,7 @@ class TCPROSTransport(Transport):
                 rospydebug("unknown socket error writing data: %s",traceback.format_exc())
                 logdebug("[%s]: closing connection [%s] due to unknown socket error: %s", self.name, self.endpoint_id, msg) 
                 self.close()
-                raise TransportTerminated(str(errno)+' '+msg)                
+                raise TransportTerminated(str(se_errno)+' '+msg)
         return True
     
     def receive_once(self):

clients/rospy/src/rospy/impl/registration.py
@@ -36,6 +36,7 @@
 
 
 
+import errno
 import socket
 import sys
 import logging
@@ -373,8 +374,8 @@ class RegManager(RegistrationListener):
                     multi.unregisterService(caller_id, resolved_name, service_uri)
             multi()
         except socket.error as se:
-            (errno, msg) = se.args
-            if errno == 111 or errno == 61: #can't talk to master, nothing we can do about it
+            (se_errno, msg) = se.args
+            if se_errno == errno.ECONNREFUSED or se_errno == errno.ENODATA: #can't talk to master, nothing we can do about it
                 self.logger.warn("cannot unregister with master due to network issues")
             else:
                 self.logger.warn("unclean shutdown\n%s"%traceback.format_exc())

tools/rosgraph/src/rosgraph/xmlrpc.py
@@ -42,6 +42,7 @@ calculation based on ROS environment variables.
 The common entry point for most libraries is the L{XmlRpcNode} class.
 """
 
+import errno
 import logging
 import select
 import socket
@@ -263,7 +264,7 @@ class XmlRpcNode(object):
             self.server.register_instance(self.handler)
 
         except socket.error as e:
-            if e.errno == 98:
+            if e.errno == errno.EADDRINUSE:
                 msg = "ERROR: Unable to start XML-RPC server, port %s is already in use"%self.port
             else:
                 msg = "ERROR: Unable to start XML-RPC server: %s" % e.strerror
@@ -291,7 +292,7 @@ class XmlRpcNode(object):
                 # exceptions break _run.
                 if self.is_shutdown:
                     pass
-                elif e.errno != 4:
+                elif e.errno != errno.EINTR:
                     self.is_shutdown = True
                     logging.getLogger('xmlrpc').error("serve forever IOError: %s, %s"%(e.errno, e.strerror))
                     

tools/roslaunch/src/roslaunch/loader.py
@@ -36,6 +36,7 @@
 General routines and representations for loading roslaunch model.
 """
 
+import errno
 import os
 from copy import deepcopy
 
@@ -495,7 +496,7 @@ class Loader(object):
                 if p.returncode != 0:
                     raise ValueError("Cannot load command parameter [%s]: command [%s] returned with code [%s]"%(name, command, p.returncode))
             except OSError as e:
-                if e.errno == 2:
+                if e.errno == errno.ENOENT:
                     raise ValueError("Cannot load command parameter [%s]: no such command [%s]"%(name, command))
                 raise
             if c_value is None:

tools/roslaunch/src/roslaunch/nodeprocess.py
@@ -36,6 +36,7 @@
 Local process implementation for running and monitoring nodes.
 """
 
+import errno
 import os
 import signal
 import subprocess 
@@ -220,7 +221,7 @@ class LocalProcess(Process):
             try:
                 os.makedirs(log_dir)
             except OSError as e:
-                if e.errno == 13:
+                if e.errno == errno.EACCES:
                     raise RLException("unable to create directory for log file [%s].\nPlease check permissions."%log_dir)
                 else:
                     raise RLException("unable to create directory for log file [%s]: %s"%(log_dir, e.strerror))
@@ -305,9 +306,9 @@ class LocalProcess(Process):
             except OSError as e:
                 self.started = True # must set so is_alive state is correct
                 _logger.error("OSError(%d, %s)", e.errno, e.strerror)
-                if e.errno == 8: #Exec format error
+                if e.errno == errno.ENOEXEC: #Exec format error
                     raise FatalProcessLaunch("Unable to launch [%s]. \nIf it is a script, you may be missing a '#!' declaration at the top."%self.name)
-                elif e.errno == 2: #no such file or directory
+                elif e.errno == errno.ENOENT: #no such file or directory
                     raise FatalProcessLaunch("""Roslaunch got a '%s' error while attempting to run:
 
 %s

commit c2e4ec6dfd578273d6603b448dc7b5eae9bb78b0

Fix for (#1224)
Fix typo introduced by commit 423209eb

clients/rospy/src/rospy/impl/tcpros_base.py
@@ -570,7 +570,7 @@ class TCPROSTransport(Transport):
                 # FATAL: no reconnection as error is unknown
                 self.close()
             elif not isinstance(e, socket.timeout) and e.errno not in [
-                    errno.ENETDOWN, e.ENETUNREACH, errno.ENETRESET,
+                    errno.ENETDOWN, errno.ENETUNREACH, errno.ENETRESET,
                     errno.ECONNABORTED, errno.ETIMEDOUT, errno.EHOSTDOWN, errno.EHOSTUNREACH]:
                 # reconnect in follow cases, otherwise close the socket:
                 # 1. socket.timeout: on timeouts caused by delays on wireless links