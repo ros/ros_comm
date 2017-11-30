c386e8eb7c4e8271fc7f78b53b4deebd5596ba96
Felix Ruess <felix.ruess@gmail.com>
2017-11-30 21:54:40 +0100

    replace clock_gettime for OSX

--- utilities/xmlrpcpp/src/XmlRpcDispatch.cpp
+++ utilities/xmlrpcpp/src/XmlRpcDispatch.cpp
@@ -223,7 +223,17 @@ XmlRpcDispatch::getTime()
 	  ((double) tbuff.timezone * 60));
 #else
   struct timespec ts;
+# ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+  clock_serv_t cclock;
+  mach_timespec_t mts;
+  host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &cclock);
+  clock_get_time(cclock, &mts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  ts.tv_sec = mts.tv_sec;
+  ts.tv_nsec = mts.tv_nsec;
+#else
   clock_gettime(CLOCK_MONOTONIC, &ts);
+#endif
   return (ts.tv_sec + ts.tv_nsec / 1000000000.0);
 #endif /* USE_FTIME */
 }
