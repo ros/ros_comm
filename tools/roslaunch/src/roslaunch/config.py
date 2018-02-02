7e7fa3f1578b375247d91d156f98ee16269bf3e4
Nick Stockton <sputnick1124@comcast.net>
2018-02-01 22:12:32 -0500

    update doc block with new roslaunch_files value options

--- tools/roslaunch/src/roslaunch/config.py
+++ tools/roslaunch/src/roslaunch/config.py
@@ -413,8 +413,8 @@ def load_config_default(roslaunch_files, port, roslaunch_strs=None, loader=None,
     roslaunch_files and or launch XML strings and initializing it. This
     config will have a core definition and also set the master to run
     on port.
-    @param roslaunch_files: list of launch files to load
-    @type  roslaunch_files: [str]
+    @param roslaunch_files: list of launch files to load. Launch files may optionally be tupled with list of args.
+    @type  roslaunch_files: [str|(str,[str])]
     @param port: roscore/master port override. Set to 0 or None to use default.
     @type  port: int
     @param roslaunch_strs: (optional) roslaunch XML strings to load
