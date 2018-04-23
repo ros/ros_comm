e1ac4cfab9d56c7f5cd19a75043a270616d66453
Mike Purvis <mpurvis@clearpathrobotics.com>
2018-04-23 17:13:24 -0400

    Revert "Use HTTP/1.1 in XMLRPC Server (#1287)"
    This reverts commit 5e2b38ea814cb9ed9437495f489d49686043b6af.
    
    This change may be considered for backporting to Lunar at a later time.

--- tools/rosgraph/src/rosgraph/xmlrpc.py
+++ tools/rosgraph/src/rosgraph/xmlrpc.py
@@ -77,9 +77,6 @@ def isstring(s):
         return isinstance(s, str)
 
 class SilenceableXMLRPCRequestHandler(SimpleXMLRPCRequestHandler):
-
-    protocol_version = 'HTTP/1.1'
-
     def log_message(self, format, *args):
         if 0:
             SimpleXMLRPCRequestHandler.log_message(self, format, *args)
@@ -89,9 +86,6 @@ class ThreadingXMLRPCServer(socketserver.ThreadingMixIn, SimpleXMLRPCServer):
     Adds ThreadingMixin to SimpleXMLRPCServer to support multiple concurrent
     requests via threading. Also makes logging toggleable.
     """
-
-    daemon_threads = True
-
     def __init__(self, addr, log_requests=1):
         """
         Overrides SimpleXMLRPCServer to set option to allow_reuse_address.
