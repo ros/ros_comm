53d958fdccb15c2c5d7ed3cef33850e2f472f7d4
Maxime St-Pierre <me@maximest-pierre.me>
2018-08-28 13:05:12 -0400

    show connection info on rosnode info (#1497)

--- tools/rosnode/src/rosnode/__init__.py
+++ tools/rosnode/src/rosnode/__init__.py
@@ -45,6 +45,7 @@ import errno
 import sys
 import socket
 import time
+import re
 try:
     from xmlrpc.client import ServerProxy
 except ImportError:
@@ -62,6 +63,8 @@ import rostopic
 
 NAME='rosnode'
 ID = '/rosnode'
+# The string is defined in clients/rospy/src/rospy/impl/tcpros_base.py TCPROSTransport.get_transport_info
+CONNECTION_PATTERN = re.compile(r'\w+ connection on port (\d+) to \[(.*) on socket (\d+)\]')
 
 class ROSNodeException(Exception):
     """
@@ -543,11 +546,16 @@ def get_node_connection_info_description(node_api, master):
                     # older ros publisher implementations don't report a URI
                     buff += "    * to: %s\n"%lookup_uri(master, system_state, topic, dest_id)
                     if direction == 'i':
-                        buff += "    * direction: inbound\n"
+                        buff += "    * direction: inbound"
                     elif direction == 'o':
-                        buff += "    * direction: outbound\n"
+                        buff += "    * direction: outbound"
                     else:
-                        buff += "    * direction: unknown\n"
+                        buff += "    * direction: unknown"
+                    if len(info) > 6:
+                        match = CONNECTION_PATTERN.match(info[6])
+                        if match is not None:
+                            buff += " (%s - %s) [%s]" % match.groups()
+                    buff += "\n"
                     buff += "    * transport: %s\n"%transport
     except socket.error:
         raise ROSNodeIOException("Communication with node[%s] failed!"%(node_api))
