57d5b4e4dd00309fafe5721713b73d4dad2df630
Martin Pecka <peckama2@fel.cvut.cz>
2018-09-07 16:30:13 +0200

    Added tests for sigint- and sigterm-timeout.

---
+++ tools/roslaunch/test/signal_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import signal
+import tempfile
+import time
+
+
+LOG_FILE = os.path.join(tempfile.gettempdir(), "signal.log")
+log_stream = open(LOG_FILE, 'w')
+
+
+def handler(signum, frame):
+    log_stream.write("%i %s\n" % (signum, str(time.time())))
+    log_stream.flush()
+
+    if signum == signal.SIGTERM:
+        log_stream.close()
+
+
+signal.signal(signal.SIGINT, handler)
+signal.signal(signal.SIGTERM, handler)
+
+while True:
+    pass
