da7bd008b37cf8abce90be6d37b40e959eacbd2f
Alexander Reimann <alexander.reimann@enway.ai>
2018-11-22 12:17:11 +0100

    [rosbag] Fix waitForSubscribers hanging with simtime
    Use Walltime for sleep as clock is not running yet

--- tools/rosbag/src/player.cpp
+++ tools/rosbag/src/player.cpp
@@ -405,7 +405,7 @@ void Player::waitForSubscribers() const
         foreach(const PublisherMap::value_type& pub, publishers_) {
             all_topics_subscribed &= pub.second.getNumSubscribers() > 0;
         }
-        ros::Duration(0.1).sleep();
+        ros::WallTime::sleepUntil(ros::WallTime::now() +  ros::WallDuration(0.1));
     }
     std::cout << "Finished waiting for subscribers." << std::endl;
 }
