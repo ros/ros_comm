0d60cb17bb3db7894d93c652268584cf5a0076d7
Martin Pecka <peci1@seznam.cz>
2019-01-29 13:32:50 +0100

    Fix a busy-wait loop in subscription queue.

--- test/test_roscpp/test/test_callback_queue.cpp
+++ test/test_roscpp/test/test_callback_queue.cpp
@@ -36,6 +36,9 @@
 #include <gtest/gtest.h>
 #include <ros/callback_queue.h>
 #include <ros/console.h>
+#include <ros/message_deserializer.h>
+#include <ros/subscription_queue.h>
+#include <ros/subscription_callback_helper.h>
 #include <ros/timer.h>
 
 #include <boost/atomic.hpp>
@@ -43,6 +46,8 @@
 #include <boost/bind.hpp>
 #include <boost/thread.hpp>
 #include <boost/function.hpp>
+
+#include "fake_message.cpp"
 
 using namespace ros;
 
@@ -258,23 +263,29 @@ TEST(CallbackQueue, recursive4)
   EXPECT_EQ(cb->count, 3U);
 }
 
-void callAvailableThread(CallbackQueue* queue, bool& done)
+void callAvailableThread(CallbackQueue* queue, bool& done, boost::atomic<size_t>* num_calls)
 {
+  size_t i = 0;
   while (!done)
   {
     queue->callAvailable(ros::WallDuration(0.1));
+    ++i;
   }
+
+  if (num_calls)
+    num_calls->fetch_add(i);
 }
 
-size_t runThreadedTest(const CountingCallbackPtr& cb, const boost::function<void(CallbackQueue*, bool&)>& threadFunc)
+size_t runThreadedTest(const CallbackInterfacePtr& cb, const boost::function<void(CallbackQueue*, bool&, boost::atomic<size_t>*)>& threadFunc, size_t* num_calls = NULL)
 {
   CallbackQueue queue;
   boost::thread_group tg;
   bool done = false;
+  boost::atomic<size_t> calls(0);
 
   for (uint32_t i = 0; i < 10; ++i)
   {
-    tg.create_thread(boost::bind(threadFunc, &queue, boost::ref(done)));
+    tg.create_thread(boost::bind(threadFunc, &queue, boost::ref(done), &calls));
   }
 
   ros::WallTime start = ros::WallTime::now();
@@ -293,6 +304,9 @@ size_t runThreadedTest(const CountingCallbackPtr& cb, const boost::function<void
   done = true;
   tg.join_all();
 
+  if (num_calls)
+    *num_calls = calls;
+
   return i;
 }
 
@@ -304,12 +318,17 @@ TEST(CallbackQueue, threadedCallAvailable)
   EXPECT_EQ(cb->count, i);
 }
 
-void callOneThread(CallbackQueue* queue, bool& done)
+void callOneThread(CallbackQueue* queue, bool& done, boost::atomic<size_t>* num_calls)
 {
+  size_t i = 0;
   while (!done)
   {
     queue->callOne(ros::WallDuration(0.1));
+    ++i;
   }
+
+  if (num_calls)
+    num_calls->fetch_add(i);
 }
 
 TEST(CallbackQueue, threadedCallOne)
@@ -318,6 +337,78 @@ TEST(CallbackQueue, threadedCallOne)
   size_t i = runThreadedTest(cb, callOneThread);
   ROS_INFO_STREAM(i);
   EXPECT_EQ(cb->count, i);
+}
+
+class CountingSubscriptionQueue : public SubscriptionQueue
+{
+public:
+    CountingSubscriptionQueue(const std::string& topic, int32_t queue_size,
+                              bool allow_concurrent_callbacks)
+        : SubscriptionQueue(topic, queue_size, allow_concurrent_callbacks),
+          ready_count(0)
+    {}
+
+    virtual bool ready() {
+      boost::mutex::scoped_lock lock(ready_mutex);
+      ++ready_count;
+      return SubscriptionQueue::ready();
+    }
+
+    boost::mutex ready_mutex;
+    size_t ready_count;
+};
+typedef boost::shared_ptr<CountingSubscriptionQueue> CountingSubscriptionQueuePtr;
+
+TEST(CallbackQueue, threadedCallOneSlow)
+{
+  // test for https://github.com/ros/ros_comm/issues/1545
+  // "roscpp multithreaded spinners eat up CPU when callbacks take too long"
+
+  // create a subscription queue counting the number of ready() calls and with limited
+  // queue_size so that most of the messages should be thrown away
+  const int32_t queue_size = 3;
+  const CountingSubscriptionQueuePtr cb(
+      boost::make_shared<CountingSubscriptionQueue>("test", queue_size, false));
+
+  // create a slow subscription callback (takes 1 second to complete)
+  const FakeSubHelperPtr helper(boost::make_shared<FakeSubHelper>());
+  helper->cb_ = boost::bind(&ros::WallDuration::sleep, ros::WallDuration(1.0));
+  const MessageDeserializerPtr des(boost::make_shared<MessageDeserializer>(
+      helper, SerializedMessage(), boost::shared_ptr<M_string>()));
+
+  // put 10 callbacks on the subscription queue (more than we can process in 5 secs)
+  for (size_t i = 0; i < 10; ++i)
+    cb->push(helper, des, false, VoidConstWPtr(), true);
+
+  // keep filling the callback queue at maximum speed for 5 seconds and
+  // spin up 10 processing threads until the queue is empty
+  size_t num_call_one_calls = 0;
+  const size_t num_callbacks_to_call = runThreadedTest(cb, callOneThread, &num_call_one_calls);
+
+  const int32_t num_callbacks_called = helper->calls_;
+  const size_t num_ready_called = cb->ready_count;
+
+  // what should happen: even though we have multiple processing threads,
+  // the subscription queue has a per-topic lock which prevents multiple threads from
+  // processing the topic's callbacks simultaneously; so even though there were
+  // tens of thousands of callbacks in the callback queue, we only got time to process
+  // less than 5 of them because queue_size is quite limited (3), so most callbacks
+  // get thrown away during processing of the slow callbacks
+
+  // we test the number of SubscriptionQueue::ready() calls to see how often do the
+  // idle threads ask for more work; with bug 1545 unfixed, this gets to millions of
+  // calls which acts as a busy-wait; with the bug fixed, the number should not be
+  // higher than number of callbacks (~ 80k), since each newly added callback should
+  // wake one idle thread and let it ask for work
+
+  ROS_INFO_STREAM("Callback queue called " <<
+                  num_callbacks_called << " / " <<
+                  num_callbacks_to_call <<  " / " << num_call_one_calls << " callbacks. Ready called " <<
+                  num_ready_called << " times.");
+
+  EXPECT_GE(num_callbacks_called, 3);
+  EXPECT_LE(num_callbacks_called, 5);
+  EXPECT_LE(num_call_one_calls, 3 * num_callbacks_to_call);
 }
 
 // this class is just an ugly hack
@@ -384,10 +475,11 @@ TEST(CallbackQueue, recursiveTimer)
 
   boost::thread_group tg;
   bool done = false;
+  boost::atomic<size_t> calls(0);
 
   for (uint32_t i = 0; i < 2; ++i)
   {
-    tg.create_thread(boost::bind(callOneThread, &queue, boost::ref(done)));
+    tg.create_thread(boost::bind(callOneThread, &queue, boost::ref(done), &calls));
   }
 
   while (!queue.isEmpty())
