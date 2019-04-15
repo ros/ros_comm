9e49232ba254a207c353c3a01678c8ae1968ab8b
Tahsincan Köse <tahsinkose@yandex.com>
2019-04-15 22:00:56 +0300

    Resolve memory leak. (#1503)
    * Resolve memory leak.
    
    Delete g_rosout_appender explicitly instead of assigning it to NULL.
    Follow deletion with NULL assignment.
    
    * Deregister g_rosout_appender
    
    * revert unrelated whitespace change
    
    * Update init.cpp
    
    * Increment version number.
    
    * space in comments
    
    * Merge
    
    * Revert "Increment version number."
    
    This reverts commit 795c8fda33821b635ad30a4828d9071bcc69bed4.
    
    * Add newer rosconsole dependencies
    
    * Update rosconsole dependencies in package.xml
    
    * Sync with upstream completely
    
    * Add changes
    
    * Remove deregister function, since it is already done in shutdown().
    
    * Remove unnecessary .catkin_workspace file.

--- clients/roscpp/src/libros/init.cpp
+++ clients/roscpp/src/libros/init.cpp
@@ -595,7 +595,8 @@ void shutdown()
   {
     g_internal_queue_thread.join();
   }
-
+  //ros::console::deregister_appender(g_rosout_appender);
+  delete g_rosout_appender;
   g_rosout_appender = 0;
 
   if (g_started)
