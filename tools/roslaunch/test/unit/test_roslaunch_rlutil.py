1dc55cacac4cd26e528d89216b31a84d6229d118
collin-scribner <collinscribner13@gmail.com>
2019-07-29 18:37:38 -0500

    Add unit test for ignore-default-args option

--- tools/roslaunch/test/unit/test_roslaunch_rlutil.py
+++ tools/roslaunch/test/unit/test_roslaunch_rlutil.py
@@ -89,3 +89,13 @@ class TestRoslaunchRlutil(unittest.TestCase):
         filename = os.path.join(get_example_path(), 'example-pass_all_args.launch')
         error_msg = roslaunch.rlutil.check_roslaunch(filename)
         assert error_msg is None
+        
+    def test_check_roslaunch(self):
+        from roslaunch.rlutil import check_roslaunch
+
+        test_ignore_default_args_p = os.path.join(get_test_path(), 'test', 'xml', 'test-ignore-default-args.launch')
+        try:
+            result = check_roslaunch(test_ignore_default_args_p, ignore_default_args=True)
+            self.assertEqual(result, None)
+        except roslaunch.RLException:
+            pass
