cbfc5df3b8b3bf1214bfab555c590745161da0b9
Johannes Meyer <meyer@fsr.tu-darmstadt.de>
2019-12-22 01:08:10 +0100

    Revert "fix base64 encode error (#1769)"
    This reverts commit ec8b0ef34a7d8063066af27aee33ac559b3043d1.

--- utilities/xmlrpcpp/libb64/src/cdecode.c
+++ utilities/xmlrpcpp/libb64/src/cdecode.c
@@ -7,7 +7,7 @@ For details, see http://sourceforge.net/projects/libb64
 
 #include <b64/cdecode.h>
 
-int base64_decode_value(signed char value_in)
+int base64_decode_value(char value_in)
 {
 	static const char decoding[] = {62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1,-1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
 	static const char decoding_size = sizeof(decoding);
@@ -26,7 +26,7 @@ int base64_decode_block(const char* code_in, const int length_in, char* plaintex
 {
 	const char* codechar = code_in;
 	char* plainchar = plaintext_out;
-	signed char fragment;
+	char fragment;
 	
 	if(length_in == 0) {
     return 0;
