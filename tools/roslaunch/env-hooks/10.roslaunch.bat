cc543dce81c2bae4e655640196acc0fb74adad62
seanyen <seanyen@microsoft.com>
2020-01-28 12:35:54 -0800

    workaround for Python 2 xmlrpc performance issues.

--- tools/roslaunch/env-hooks/10.roslaunch.bat
+++ tools/roslaunch/env-hooks/10.roslaunch.bat
@@ -1,5 +1,12 @@
 REM roslaunch/env-hooks/10.roslaunch.bat
 
+:: workaround Python 2 xmlrpc performance issue
+:: https://stackoverflow.com/questions/2617615/slow-python-http-server-on-localhost
+:: use IP address instead to avoid unnecessary DNS lookups.
 if "%ROS_MASTER_URI%" == "" (
-  set ROS_MASTER_URI=http://localhost:11311
+  set ROS_MASTER_URI=http://127.0.0.1:11311
+)
+
+if "%ROS_IP%" == "" (
+  set ROS_IP=127.0.0.1
 )
