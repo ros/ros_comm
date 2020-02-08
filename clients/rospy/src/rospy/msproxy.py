d16296acb755880cb1399c483895aea05aec6f0e
Markus Grimm <grimm@magazino.eu>
2020-02-08 05:15:27 +0100

    rospy: added get_param_cached (#1515)
    * added get_param_cached
    
    * Avoid unnecessary whitespace change
    
    * Fixes for broken tests
    
    * Fixes for failing test
    
    * Fixed behavior for unset keys
    
    * Use cache when possible
    
    * Added missing lock

--- clients/rospy/src/rospy/msproxy.py
+++ clients/rospy/src/rospy/msproxy.py
@@ -116,25 +116,15 @@ class MasterProxy(object):
         """
         #NOTE: remapping occurs here!
         resolved_key = rospy.names.resolve_name(key)
-        if 1: # disable param cache
-            with self._lock:
-                code, msg, value = self.target.getParam(rospy.names.get_caller_id(), resolved_key)
-            if code != 1: #unwrap value with Python semantics
-                raise KeyError(key)
-            return value
-
-        try:
-            # check for value in the parameter server cache
-            return rospy.impl.paramserver.get_param_server_cache().get(resolved_key)
-        except KeyError:
-            # first access, make call to parameter server
-            with self._lock:
-                code, msg, value = self.target.subscribeParam(rospy.names.get_caller_id(), rospy.core.get_node_uri(), resolved_key)
-            if code != 1: #unwrap value with Python semantics
-                raise KeyError(key)
-            # set the value in the cache so that it's marked as subscribed
-            rospy.impl.paramserver.get_param_server_cache().set(resolved_key, value)
-            return value
+        with self._lock:
+            try:
+                return rospy.impl.paramserver.get_param_server_cache().get(resolved_key)
+            except KeyError:
+                pass
+            code, msg, value = self.target.getParam(rospy.names.get_caller_id(), resolved_key)
+        if code != 1: #unwrap value with Python semantics
+            raise KeyError(key)
+        return value
         
     def __setitem__(self, key, val):
         """
@@ -166,6 +156,23 @@ class MasterProxy(object):
             return None
         else:
             raise rospy.exceptions.ROSException("cannot search for parameter: %s"%msg)
+
+    def get_param_cached(self, key):
+        resolved_key = rospy.names.resolve_name(key)
+        try:
+            # check for value in the parameter server cache
+            return rospy.impl.paramserver.get_param_server_cache().get(resolved_key)
+        except KeyError:
+            # first access, make call to parameter server
+            with self._lock:
+                code, msg, value = self.target.subscribeParam(rospy.names.get_caller_id(), rospy.core.get_node_uri(), resolved_key)
+            if code != 1: #unwrap value with Python semantics
+                raise KeyError(key)
+            # set the value in the cache so that it's marked as subscribed
+            rospy.impl.paramserver.get_param_server_cache().set(resolved_key, value)
+            if isinstance(value, dict) and not value:
+                raise KeyError(key)
+            return value
         
     def __delitem__(self, key):
         """
@@ -180,9 +187,6 @@ class MasterProxy(object):
             raise KeyError(key)
         elif code != 1:
             raise rospy.exceptions.ROSException("cannot delete parameter: %s"%msg)
-        elif 0: #disable parameter cache
-            # set the value in the cache so that it's marked as subscribed
-            rospy.impl.paramserver.get_param_server_cache().delete(resolved_key)
 
     def __contains__(self, key):
         """
