03a743892a6603ce28425527c60a5d94d7dfe0cd
Barry Xu <barry.xu@sony.com>
2020-05-22 02:19:47 +0800

    Fix a bug that using a destroyed connection object. (#1950)
    there is a case that accessing a connection object which was destroyed by
    ConnectionManager and TransportSubscriberLink/TransportPublicationLink
    while getting a close event in pollset to call Connection::drop.
    
    Signed-off-by: Barry Xu <Barry.Xu@sony.com>

--- clients/roscpp/src/libros/connection.cpp
+++ clients/roscpp/src/libros/connection.cpp
@@ -337,11 +337,11 @@ void Connection::drop(DropReason reason)
 
   if (did_drop)
   {
+    transport_->close();
     {
       boost::recursive_mutex::scoped_lock lock(drop_mutex_);
       drop_signal_(shared_from_this(), reason);
     }
-    transport_->close();
   }
 }
 
