bbca98b448ff236d33e0075593e46d55c9a6b5d0
FunkyMax <max-bauer97@web.de>
2020-07-26 14:40:36 +0200

    rosnode_cleanup() now also removes unavailable nodes from the local node cache _caller_apis. This prevents buggy behaviour when nodes become unvailable because they exit the ros network due to wifi issues but then reconnect to it later on.

--- tools/rosnode/src/rosnode/__init__.py
+++ tools/rosnode/src/rosnode/__init__.py
@@ -406,7 +406,7 @@ def rosnode_ping_all(verbose=False):
     
 def cleanup_master_blacklist(master, blacklist):
     """
-    Remove registrations from ROS Master that match blacklist.    
+    Remove registrations from ROS Master and node cache (_caller_apis) that match blacklist.    
     @param master: rosgraph Master instance
     @type  master: rosgraph.Master
     @param blacklist: list of nodes to scrub
@@ -429,6 +429,7 @@ def cleanup_master_blacklist(master, blacklist):
                 service_api = master.lookupService(s)
                 master_n = rosgraph.Master(n)
                 master_n.unregisterService(s, service_api)
+		_caller_apis.pop(n, None)
 
 def cleanup_master_whitelist(master, whitelist):
     """
