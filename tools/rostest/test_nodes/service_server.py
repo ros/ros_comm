833b95b01890b3fa49d8650d00db31ef5072ca29
Anthony Remazeilles <anthony.remazeilles@tecnalia.com>
2020-08-05 12:32:43 +0200

    rostest: add service providing some results output

--- tools/rostest/test_nodes/service_server.py
+++ tools/rostest/test_nodes/service_server.py
@@ -14,10 +14,19 @@ def empty_cb(req):
     return EmptyResponse()
 
 def set_bool_cb(req):
-    return SetBoolResponse()
+    response = SetBoolResponse()
+    response.success = True
+    response.message = str(req.data)
+    return response
 
 def trigger_cb(req):
     return TriggerResponse()
+
+def trigger2_cb(req):
+    response = TriggerResponse()
+    response.success = True
+    response.message = 'well done!'
+    return response
 
 def main():
     rospy.init_node('service_server')
@@ -25,6 +34,7 @@ def main():
     empty_service = rospy.Service('empty', Empty, empty_cb)
     set_bool_service = rospy.Service('set_bool', SetBool, set_bool_cb)
     trigger_service = rospy.Service('trigger', Trigger, trigger_cb)
+    trigger2_service = rospy.Service('trigger_spec', Trigger, trigger2_cb)
 
     rospy.spin()
 
