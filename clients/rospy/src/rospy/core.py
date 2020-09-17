b197fbe1ae5c925e21237d7af0b6f2c4a5f3fb31
salihmarangoz <12991962+salihmarangoz@users.noreply.github.com>
2020-09-18 01:39:38 +0300

    Fix log*_throttle with sim time (#2044)
    * Fix log*_throttle with sim time
    
    When log*_throttle is used with bags or a simulation, logging stops. Solved this problem with resetting logging_time_table when ros time moved backward
    
    * fixed a bug where client loses a log message when rostime goes backward
    
    removed paranthesis
    fixed a bug where client loses a log message when rostime goes backward

--- clients/rospy/src/rospy/core.py
+++ clients/rospy/src/rospy/core.py
@@ -176,6 +176,10 @@ class LoggingThrottle(object):
               (now - last_logging_time) > rospy.Duration(period)):
             logging_func(msg)
             self.last_logging_time_table[caller_id] = now
+        elif last_logging_time > now:
+            logging_func(msg)
+            self.last_logging_time_table = {}
+            self.last_logging_time_table[caller_id] = now
 
 
 _logging_throttle = LoggingThrottle()
