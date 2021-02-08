6adf779db1347876b3cf0b4918b03c8a6f952f9c
229143434@qq.com <zxcvbnm123-*@>
2021-02-08 10:57:16 +0800

    fix bug #2123

--- clients/rospy/src/rospy/impl/tcpros_base.py
+++ clients/rospy/src/rospy/impl/tcpros_base.py
@@ -63,6 +63,7 @@ from rospy.msg import deserialize_messages, serialize_message
 from rospy.service import ServiceException
 
 from rospy.impl.transport import Transport, BIDIRECTIONAL
+from errno import EAGAIN, EWOULDBLOCK
 
 logger = logging.getLogger('rospy.tcpros')
 
@@ -101,12 +102,18 @@ def recv_buff(sock, b, buff_size):
     @return: number of bytes read
     @rtype: int
     """
-    d = sock.recv(buff_size)
-    if d:
-        b.write(d)
-        return len(d)
-    else: #bomb out
-        raise TransportTerminated("unable to receive data from sender, check sender's logs for details")
+    try:
+        d = sock.recv(buff_size)
+        if d:
+            b.write(d)
+            return len(d)
+        else: #bomb out
+            raise TransportTerminated("unable to receive data from sender, check sender's logs for details")
+    except socket.error as ex:
+        if ex.errno not in (EAGAIN, EWOULDBLOCK):
+            raise TransportTerminated("unable to receive data from sender, check sender's logs for details")
+        else:
+            return 0
 
 class TCPServer(object):
     """
@@ -799,6 +806,11 @@ class TCPROSTransport(Transport):
                             msgs_callback(msgs, self)
                     else:
                         self._reconnect()
+
+                except TransportTerminated as e:
+                    logdebug("[%s] failed to receive incoming message : %s"%(self.name, str(e)))
+                    rospydebug("[%s] failed to receive incoming message: %s"%(self.name, traceback.format_exc()))
+                    break
 
                 except TransportException as e:
                     # set socket to None so we reconnect
