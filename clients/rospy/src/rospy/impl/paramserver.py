ros/ros_comm

commit d16296acb755880cb1399c483895aea05aec6f0e

rospy: added get_param_cached (#1515)
* added get_param_cached

* Avoid unnecessary whitespace change

* Fixes for broken tests

* Fixes for failing test

* Fixed behavior for unset keys

* Use cache when possible

* Added missing lock

clients/rospy/src/rospy/impl/paramserver.py
@@ -34,8 +34,8 @@
 
 
 import threading
+from rosgraph.names import GLOBALNS, SEP
 
-#TODO: get rid of this routine entirely. It doesn't work with the current PS representation.
 class ParamServerCache(object):
     """
     Cache of values on the parameter server. Implementation
@@ -44,13 +44,35 @@ class ParamServerCache(object):
     
     def __init__(self):
         self.lock = threading.Lock()
-        self.d = {}
+        self.d = None
         self.notifier = None
         
     ## Delete parameter from cache
     def delete(self, key):
         with self.lock:
-            del self.d[key]
+            # partially borrowed from rosmaster/paramserver.py
+            if key == GLOBALNS:
+                raise KeyError("cannot delete root of parameter tree")
+            elif self.d is None:
+                raise KeyError(key)
+            else:
+                # key is global, so first split is empty
+                namespaces = [x for x in key.split(SEP) if x]
+                # - last namespace is the actual key we're deleting
+                value_key = namespaces[-1]
+                namespaces = namespaces[:-1]
+                d = self.d
+                # - descend tree to the node we're setting
+                for ns in namespaces:
+                    if type(d) != dict or ns not in d:
+                        raise KeyError(key)
+                    else:
+                        d = d[ns]
+
+                if value_key not in d:
+                    raise KeyError(key)
+                else:
+                    del d[value_key]
 
     def set_notifier(self, notifier):
         """
@@ -70,16 +92,26 @@ class ParamServerCache(object):
         @type  value: str
         @raise: KeyError if key is not already in the cache.
         """
-        # TODOXXX: remove this code, as it is wrong. It is remaining
-        # in place to maintain a unit test until the correct logic is
-        # implemented. update() needs to check all subscribed keys and
-        # do a prefix match. The best way to do this is probably to
-        # pull the paramserver implementation from rosmaster and use
-        # it here.
-        if not key in self.d:
-            raise KeyError(key)
         with self.lock:
-            self.d[key] = value
+            # partially borrowed from rosmaster/paramserver.py
+            namespaces = [x for x in key.split(SEP) if x]
+            # - last namespace is the actual key we're storing in
+            value_key = namespaces[-1]
+            namespaces = namespaces[:-1]
+            d = self.d
+            if d is None:
+                raise KeyError(key)
+            # - descend tree to the node we're setting
+            for ns in namespaces:
+                if ns not in d:
+                    raise KeyError(key)
+                else:
+                    d = d[ns]
+
+            if value_key not in d:
+                raise KeyError(key)
+
+            d[value_key] = value
             if self.notifier is not None:
                 self.notifier(key, value)
                 
@@ -93,8 +125,35 @@ class ParamServerCache(object):
         @type  value: str
         """
         with self.lock:
-            self.d[key] = value
-            
+            # partially borrowed from rosmaster/paramserver.py
+            if key == GLOBALNS:
+                if type(value) != dict:
+                    raise TypeError("cannot set root of parameter tree to "
+                                    "non-dictionary")
+                self.d = value
+            else:
+                namespaces = [x for x in key.split(SEP) if x]
+                # - last namespace is the actual key we're storing in
+                value_key = namespaces[-1]
+                namespaces = namespaces[:-1]
+                if self.d is None:
+                    self.d = {}
+                d = self.d
+                # - descend tree to the node we're setting
+                for ns in namespaces:
+                    if ns not in d:
+                        new_d = {}
+                        d[ns] = new_d
+                        d = new_d
+                    else:
+                        val = d[ns]
+                        # implicit type conversion of value to namespace
+                        if type(val) != dict:
+                            d[ns] = val = {}
+                        d = val
+
+                d[value_key] = value
+
     def get(self, key):
         """
         @param key: parameter key
@@ -103,7 +162,20 @@ class ParamServerCache(object):
         @raise: KeyError
         """
         with self.lock:
-            return self.d[key]
+            # borrowed from rosmaster/paramserver.py
+            if self.d is None:
+                raise KeyError(key)
+            val = self.d
+            if key != GLOBALNS:
+                # split by the namespace separator, ignoring empty splits
+                namespaces = [x for x in key.split(SEP) if x]
+                for ns in namespaces:
+                    if not type(val) == dict:
+                        raise KeyError(val)
+                    val = val[ns]
+            if isinstance(val, dict) and not val:
+                raise KeyError(key)
+            return val
 
 _param_server_cache = None
 def get_param_server_cache():

clients/rospy/src/rospy/__init__.py
@@ -51,7 +51,7 @@ from .client import spin, myargv, init_node, \
     wait_for_message, \
     get_master, \
     on_shutdown, \
-    get_param, get_param_names, set_param, delete_param, has_param, search_param,\
+    get_param, get_param_cached, get_param_names, set_param, delete_param, has_param, search_param,\
     DEBUG, INFO, WARN, ERROR, FATAL
 from .timer import sleep, Rate, Timer
 from .core import is_shutdown, signal_shutdown, \
@@ -85,6 +85,7 @@ __all__ = [
     'wait_for_service',
     'on_shutdown',
     'get_param',
+    'get_param_cached',
     'get_param_names',
     'set_param',
     'delete_param',

clients/rospy/src/rospy/client.py
@@ -471,6 +471,29 @@ def get_param(param_name, default=_unspecified):
         else:
             raise
 
+
+def get_param_cached(param_name, default=_unspecified):
+    """
+    Retrieve a parameter from the param server with local caching
+
+    NOTE: this method is thread-safe.
+
+    @param default: (optional) default value to return if key is not set
+    @type  default: any
+    @return: parameter value
+    @rtype: XmlRpcLegalValue
+    @raise ROSException: if parameter server reports an error
+    @raise KeyError: if value not set and default is not given
+    """
+    try:
+        _init_param_server()
+        return _param_server.get_param_cached(param_name)
+    except KeyError:
+        if default != _unspecified:
+            return default
+        else:
+            raise
+
 def get_param_names():
     """
     Retrieve list of parameter names.

clients/rospy/src/rospy/msproxy.py
@@ -116,25 +116,15 @@ class MasterProxy(object):
         """
         #NOTE: remapping occurs here!
         resolved_key = rospy.names.resolve_name(key)
-        if 1: # disable param cache
-            with self._lock:
-                code, msg, value = self.target.getParam(rospy.names.get_caller_id(), resolved_key)
-            if code != 1: #unwrap value with Python semantics
-                raise KeyError(key)
-            return value
-
-        try:
-            # check for value in the parameter server cache
-            return rospy.impl.paramserver.get_param_server_cache().get(resolved_key)
-        except KeyError:
-            # first access, make call to parameter server
-            with self._lock:
-                code, msg, value = self.target.subscribeParam(rospy.names.get_caller_id(), rospy.core.get_node_uri(), resolved_key)
-            if code != 1: #unwrap value with Python semantics
-                raise KeyError(key)
-            # set the value in the cache so that it's marked as subscribed
-            rospy.impl.paramserver.get_param_server_cache().set(resolved_key, value)
-            return value
+        with self._lock:
+            try:
+                return rospy.impl.paramserver.get_param_server_cache().get(resolved_key)
+            except KeyError:
+                pass
+            code, msg, value = self.target.getParam(rospy.names.get_caller_id(), resolved_key)
+        if code != 1: #unwrap value with Python semantics
+            raise KeyError(key)
+        return value
         
     def __setitem__(self, key, val):
         """
@@ -166,6 +156,23 @@ class MasterProxy(object):
             return None
         else:
             raise rospy.exceptions.ROSException("cannot search for parameter: %s"%msg)
+
+    def get_param_cached(self, key):
+        resolved_key = rospy.names.resolve_name(key)
+        try:
+            # check for value in the parameter server cache
+            return rospy.impl.paramserver.get_param_server_cache().get(resolved_key)
+        except KeyError:
+            # first access, make call to parameter server
+            with self._lock:
+                code, msg, value = self.target.subscribeParam(rospy.names.get_caller_id(), rospy.core.get_node_uri(), resolved_key)
+            if code != 1: #unwrap value with Python semantics
+                raise KeyError(key)
+            # set the value in the cache so that it's marked as subscribed
+            rospy.impl.paramserver.get_param_server_cache().set(resolved_key, value)
+            if isinstance(value, dict) and not value:
+                raise KeyError(key)
+            return value
         
     def __delitem__(self, key):
         """
@@ -180,9 +187,6 @@ class MasterProxy(object):
             raise KeyError(key)
         elif code != 1:
             raise rospy.exceptions.ROSException("cannot delete parameter: %s"%msg)
-        elif 0: #disable parameter cache
-            # set the value in the cache so that it's marked as subscribed
-            rospy.impl.paramserver.get_param_server_cache().delete(resolved_key)
 
     def __contains__(self, key):
         """

commit 84a3ce85079ee43573d68bf6b2285b0249ea39b8

rospy: Fix severe bug in get_param_cached()
This bug is severe because it can break ordinary rospy.get_param()
after a rospy.get_param_cached() has been perfomed. And, furthermore,
rospy.log*() does a rospy.get_param_cached()!

When a parameter is set in the cache, the cached value will always be
returned, even when the caller uses ordinary rospy.get_param(). This is
as designed, as subscribeParam() is done to make the cache
receive updates from the param server whenever the value changes.

However, existing code only uses the presence of a key in the cache to
indicate whether the cached param is valid. However, this is not enough.

Suppose the param server has these contents:
    top:
        min: 1
        max: 10

    print(rospy.get_param('/top'))
    # So far, so good. Output is:
    # {'min': 1, 'max': 10}

    print(rospy.get_param_cached('/top/min'))
    # As expected, this outputs:
    # 1
    # However, ParamServerCache now contains:
    # {'top': {'min': 1}}
    # This is correct, because only the value of 'min' is cached.

    print(rospy.get_param('/top'))
    # Incorrect output:
    # {'min': 1}
    # Ugh! 'max': 10 is missing!
    # ...because it looks like /top is in the cache, but it was never
    # actually cached; only the individual item within it, 'min', was
    # cached.

The fix is to track the actual keys that are cached in a Python set().
Only return a cached value if either the key or a parent of it is in
the set.

This was found by production code which attempts to get all params using
rospy.get_param('/'). This works OK as long as no calls to
rospy.get_param_cached() have been made. However, the rospy.log*()
functions all do
rospy.get_param_cached('rosout_disable_topics_generation'). So after the
first loginfo, subsequent calls to rospy.get_param('/') return only the
single cached key rather than all params.

Fixes: d16296acb755 ("rospy: added get_param_cached (#1515)")

clients/rospy/src/rospy/impl/paramserver.py
@@ -46,7 +46,8 @@ class ParamServerCache(object):
         self.lock = threading.Lock()
         self.d = None
         self.notifier = None
-        
+        self.subscribed_params = set()
+
     ## Delete parameter from cache
     def delete(self, key):
         with self.lock:
@@ -94,13 +95,13 @@ class ParamServerCache(object):
         """
         with self.lock:
             # partially borrowed from rosmaster/paramserver.py
-            namespaces = [x for x in key.split(SEP) if x]
+            path_elems = tuple(x for x in key.split(SEP) if x)
             # - last namespace is the actual key we're storing in
             d = self.d
             if d is None:
                 raise KeyError(key)
-            value_key = namespaces[-1]
-            namespaces = namespaces[:-1]
+            value_key = path_elems[-1]
+            namespaces = path_elems[:-1]
             # - descend tree to the node we're setting
             for ns in namespaces:
                 if ns not in d:
@@ -131,11 +132,12 @@ class ParamServerCache(object):
                     raise TypeError("cannot set root of parameter tree to "
                                     "non-dictionary")
                 self.d = value
+                self.subscribed_params.add(GLOBALNS)
             else:
-                namespaces = [x for x in key.split(SEP) if x]
+                path_elems = tuple(x for x in key.split(SEP) if x)
                 # - last namespace is the actual key we're storing in
-                value_key = namespaces[-1]
-                namespaces = namespaces[:-1]
+                value_key = path_elems[-1]
+                namespaces = path_elems[:-1]
                 if self.d is None:
                     self.d = {}
                 d = self.d
@@ -153,6 +155,7 @@ class ParamServerCache(object):
                         d = val
 
                 d[value_key] = value
+                self.subscribed_params.add(path_elems)
 
     def get(self, key):
         """
@@ -166,13 +169,24 @@ class ParamServerCache(object):
             if self.d is None:
                 raise KeyError(key)
             val = self.d
-            if key != GLOBALNS:
-                # split by the namespace separator, ignoring empty splits
-                namespaces = [x for x in key.split(SEP) if x]
-                for ns in namespaces:
-                    if not type(val) == dict:
-                        raise KeyError(val)
-                    val = val[ns]
+            # split by the namespace separator, ignoring empty splits
+            namespaces = tuple(x for x in key.split(SEP) if x)
+
+            if GLOBALNS not in self.subscribed_params:
+                # Make sure this key or some parent of it was actually set
+                # so that updates are subscribed.
+                is_subscribed = False
+                for i in range(len(namespaces), 0, -1):
+                    if namespaces[:i] in self.subscribed_params:
+                        is_subscribed = True
+                        break
+                if not is_subscribed:
+                    raise KeyError(key)
+
+            for ns in namespaces:
+                if not type(val) == dict:
+                    raise KeyError(val)
+                val = val[ns]
             if isinstance(val, dict) and not val:
                 raise KeyError(key)
             return val