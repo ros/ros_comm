663d03aa9c23ee689ba0b65818727017a0dfe780
Martin Pecka <peckama2@fel.cvut.cz>
2022-01-20 11:41:42 +0100

    Fix printing XmlRpcValue with GTest

--- utilities/xmlrpcpp/include/xmlrpcpp/XmlRpcValue.h
+++ utilities/xmlrpcpp/include/xmlrpcpp/XmlRpcValue.h
@@ -201,6 +201,15 @@ namespace XmlRpc {
     } _value;
     
   };
+  
+  // This prevents GTest to understand all XmlRpcValues as structs and trying to print them as such.
+  // That results in assertStruct() being called an exception being thrown in GTest EXPECT* methods.
+  void PrintTo(const XmlRpcValue& value, ::std::ostream* os)
+  {
+    if (os)
+      *os << value.toXml();
+  }
+
 } // namespace XmlRpc
 
 
