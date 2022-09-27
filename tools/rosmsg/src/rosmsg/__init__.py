46f9ef6c9c6ff27717140bfae480f70c5364d3ec
Duane Norris <duane_norris@jabil.com>
2022-09-27 13:46:15 -0400

    Fix python3.10 collections
    python 3.10
    
    collections abstract base classes are now under
    collections.abc in python 3.10.

--- tools/rosmsg/src/rosmsg/__init__.py
+++ tools/rosmsg/src/rosmsg/__init__.py
@@ -75,7 +75,7 @@ def construct_ordered_mapping(self, node, deep=False):
     mapping = collections.OrderedDict()
     for key_node, value_node in node.value:
         key = self.construct_object(key_node, deep=deep)
-        if not isinstance(key, collections.Hashable):
+        if not isinstance(key, collections.abc.Hashable):
             raise yaml.constructor.ConstructorError("while constructing a mapping", node.start_mark,
                     "found unhashable key", key_node.start_mark)
         value = self.construct_object(value_node, deep=deep)
